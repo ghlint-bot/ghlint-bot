24dcb037f4b54af0993e7bafa99e2737508bb5b3
Stephen Davis <davi1138@purdue.edu>
2020-01-24 15:57:32 -0500

    fixed variable scope bug

--- src/ghlinter/config_manager.py
+++ src/ghlinter/config_manager.py
@@ -5,10 +5,11 @@ __CONFIG_FILE_LOCATION = 'config.json'
 __CONFIGS: json = {}
 
 def load() -> None:
+    global __CONFIGS
     __CONFIGS = load_json_file(__CONFIG_FILE_LOCATION)
 
 def bot_name() -> str:
-    return __CONFIGS['bot_name']
+    return __CONFIGS["bot_name"]
 
 def issues() -> json:
     return __CONFIGS['issues']
