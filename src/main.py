67ad0c45a88d044b3ad60ddb7b2940cb5b665575
Stephen Davis <davi1138@purdue.edu>
2020-01-24 16:23:31 -0500

    cleaned up main.py

--- src/main.py
+++ src/main.py
@@ -10,116 +10,40 @@ from gidgethub import aiohttp as gh_aiohttp
 from ghlinter.issue import Issue
 from ghlinter import config_manager
 from ghlinter import issue_events
-#temp
-from ghlinter.store import store
 
-# GLOBAL variable setup
+#### router setup
 routes = web.RouteTableDef()
 router = routing.Router()
 
-#### GLOBAL User Variables
-## available user variables when creating commands and responses
-##
-## author:      author of the given issue or pull request
-## number:      number of the given issue or pull request
-## assignees:   list of users assigned to given issue or pull request
-## unassignee:  user unassigned from given issue or pull request (only available on unassigned events)
-## bot_name:    name of the bot (ghlint-bot)
-# class Store:
-#     def __init__(self):
-#         self.author = ''
-#         self.number = 0
-#         self.assignees = []
-#         self.unassignee = ''
-
-#     def format_string(self, string: str) -> str:
-#         return string.format(
-#         author = self.author, 
-#         number=self.number, 
-#         assignees=self.assignees, 
-#         unassignee=self.unassignee, 
-#         bot_name=config_manager.bot_name()
-#     )
-
-# store: Store = Store()
-
-# def format_user_globals(src: str) -> str:
-#     return src.format(
-#         author = global_variable_store.author, 
-#         number=global_variable_store.number, 
-#         assignees=global_variable_store.assignees, 
-#         unassignee=global_variable_store.unassignee, 
-#         bot_name=config_manager.bot_name()
-#     )
-
+#### issue routes
 @router.register("issues", action="opened")
 async def issue_opened_event(event: sansio.Event, github_object: gh_aiohttp.GitHubAPI, *args, **kawrgs):
     """ endpoint for when an issue is opened """
     await issue_events.opened(event, github_object)
-
-#     issue: Issue = Issue(event, github_object)
-
-#     # first check that the formatting is correct
-#     if not issue.is_formatted_correctly():
-#         await issue.comment('title is formatted incorrectly. closing issue. please resubmit your issue in the correct format')
-#         await issue.close()
-#         return
-
-#     # issue is formatted correctly post on opened comment
-#     comment: str = store.format_string(config_manager.issues()['on_opened_comment'])
-#     if comment != None:
-#         await issue.comment(comment)
 
 @router.register("issues", action="closed")
 async def issue_closed_event(event: sansio.Event, github_object: gh_aiohttp.GitHubAPI, *args, **kwargs):
     """ endpoint for when an issue is closed """
     await issue_events.closed(event, github_object)
 
-#     issue: Issue = Issue(event, github_object)
-
-#     comment: str = store.format_string(config_manager.issues()['on_closed_comment'])
-#     if comment != None:
-#         await issue.comment(comment)
-
 @router.register("issues", action="assigned")
 async def issue_assigned_event(event: sansio.Event, github_object: gh_aiohttp.GitHubAPI, *args, **kwargs):
     """ endpoint for when an issue is assigned """
     await issue_events.assigned(event, github_object)
-
-    # issue: Issue = Issue(event, github_object)
-
-#     comment: str = store.format_string(config_manager.issues()['on_assigned_comment'])
-#     if comment != None:
-#         await issue.comment(comment)
 
 @router.register("issues", action="unassigned")
 async def issue_unassigned_event(event: sansio.Event, github_object: gh_aiohttp.GitHubAPI, *args, **kwargs):
     """ endpoint for when an issue is unassigned """
     await issue_events.unassigned(event, github_object)
 
-#     issue: Issue = Issue(event, github_object)
-
-#     comment: str = store.format_string(config_manager.issues()['on_unassigned_comment'])
-#     if comment != None:
-#         await issue.comment(comment)
-
 @router.register("issue_comment", action="created")
 async def issue_commented_event(event: sansio.Event, github_object: gh_aiohttp.GitHubAPI, * args, **kwargs):
     """ endpoint for when someone comments on an issue """
     await issue_events.commented(event, github_object)
-    
-#     issue: Issue = Issue(event, github_object)
-#     comment: str = store.format_string(event.data['comment']['body'])
 
-#     # check if bot posted this comment so we can ignore
-#     if event.data['comment']['user']['login'] == config_manager.bot_name():
-#         return
+#### pull request routes
 
-#     if issue.is_registered_command(comment):
-#         await issue.dispatch(comment)
-#     else:
-#         await issue.comment(f'sorry, [{comment}] is not a registered command')
-
+#### webhook route
 @routes.post("/")
 async def main(request):
     # read the GitHub webhook payload
