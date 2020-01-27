b95e5184318ce328c8eef05d4c436887e6bdc903
Stephen Davis <davi1138@purdue.edu>
2020-01-27 16:51:14 -0500

    added debug statements

--- src/ghlinter/branch.py
+++ src/ghlinter/branch.py
@@ -2,7 +2,9 @@ import requests
 from gidgethub.aiohttp import GitHubAPI
 
 async def create_branch(repo_owner: str, repo_name: str, new_branch_name: str, github_object: GitHubAPI) -> None:
+    print(repo_owner + ' ' + repo_name + ' ' + new_branch_name)
     response = requests.get(f'https://api.github.com/repos/{repo_owner}/{repo_name}/git/refs/heads')
+    print(response.status_code)
     data = response.json()
     sha = data[0]['object']['sha']
     payload = {
